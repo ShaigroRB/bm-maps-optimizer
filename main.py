4a986607983966c3c6a06f6208a1a154adae524e
ShaigroRB <robin.boucher@epita.fr>
2021-04-07 23:14:50 +0200

    feat(parsing): get the objects as json of the map

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
+import json
+from constants import MAP_OBJECTS_INDEX, BLOCK_SIZE, BLOCKS, WALL_TOOL
 
 
 def get_lines(filepath: str) -> list[str]:
     with open(filepath, 'r') as file:
         lines = [line.rstrip() for line in file]
     return lines
+
+
+def string_to_json(string: str) -> dict:
+    _json = json.loads(string)
+    return _json
 if __name__ == '__main__':
     path = sys.argv[1]
     original_lines = get_lines(path)
+    bmap_json = string_to_json(original_lines[MAP_OBJECTS_INDEX])
