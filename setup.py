12c909cce9490cb814b104378751e961b785122d
ShaigroRB <robin.boucher@epita.fr>
2021-04-10 16:43:38 +0200

    feat(setup): add basic setup to install locally bmmo module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+VERSION = '0.1-dev'
+
+setup(
+    name='Boring Man Maps Optimizer',
+    version=VERSION,
+    packages=['bmmo'],
+    license='MIT license',
+    author='Robin Boucher',
+    url='https://github.com/ShaigroRB/bm-maps-optimizer',
+    long_description='Tool to optimize maps from the game Boring Man - OTSC.'
+)
