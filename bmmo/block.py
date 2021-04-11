adf14aec096404b4a82df5f4b5315b93b5c1309e
ShaigroRB <robin.boucher@epita.fr>
2021-04-11 22:01:51 +0200

    refactor(Block): moved Block class to its own file

---
+++ bmmo/block.py
@@ -0,0 +1,8 @@
+class Block:
+    def __init__(self, x: int, y: int, width: int, height: int, type: str, sound: str):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.type = type
+        self.sound = sound
