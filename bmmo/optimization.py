4ba1988b7d0eadd97c170fbe7c6d4ce3fc0833b3
ShaigroRB <robin.boucher@epita.fr>
2021-04-18 20:53:28 +0200

    fix(blocks optimization): algorithm not passing the tests
    There were multiple problems with the way I wrote it.
    - I was using the row and column indexes to loop but it was possible to have a situation where the row and column indexes indicating that we are at the end of the table but there is still a block left. Now, it loops as long as there is a block left. There is always one more iteration to check if there is not a block left. The test with a 4x4 table has this case taken into account.
    - Width and height of blocks could be wrongly computed because the column and row indexes were always incremented even if the current cell/row was not valid.
    - I miscomputed the range for indexes for removing blocks from the optimized table.

--- bmmo/optimization.py
+++ bmmo/optimization.py
@@ -8,17 +8,11 @@ def _is_cell_valid(cell: int) -> bool:
 
 
 def _find_coordinates_of_most_top_left_valid_cell(table: list[list[int]]) -> (int, int):
-    x = 0
-    y = 0
-
-    # refactor
     for j, row in enumerate(table):
         for i, column in enumerate(row):
             if _is_cell_valid(column):
-                x = i
-                y = j
-                return x, y
-    return x, y
+                return i, j
+    return -1, -1
 
 
 def _is_row_start_to_end_valid(table: list[list[int]], row_index: int, start_index: int, end_index: int) -> bool:
@@ -31,8 +25,8 @@ def _is_row_start_to_end_valid(table: list[list[int]], row_index: int, start_ind
 
 
 def _remove_block_from_table(block: Block, table: list[list[int]]):
-    for j in range(block.y, block.height):
-        for i in range(block.x, block.width):
+    for j in range(block.y, block.height + block.y):
+        for i in range(block.x, block.width + block.x):
             table[j][i] = 0
 
 
@@ -55,12 +49,10 @@ def get_least_blocks_from_blocks_table(blocks_table: BlocksTable) -> list[Block]
     def create_block(x: int, y: int, w: int, h: int) -> Block:
         return Block(x, y, w, h, block_type, block_sound)
 
-    index_row = 0
-    index_col = 0
+    top_left_cell_x, top_left_cell_y = _find_coordinates_of_most_top_left_valid_cell(optimized_table)
+    index_row, index_col = top_left_cell_y, top_left_cell_x + 1
 
-    while (index_row < nb_rows) and (index_col < nb_columns):
-        top_left_cell_x, top_left_cell_y = _find_coordinates_of_most_top_left_valid_cell(optimized_table)
-        index_row, index_col = top_left_cell_y, top_left_cell_x + 1
+    while (top_left_cell_x != -1) and (top_left_cell_y != -1):
         current_cell_is_valid = True
 
         # get the index of the first invalid cell from left to right
@@ -68,7 +60,8 @@ def get_least_blocks_from_blocks_table(blocks_table: BlocksTable) -> list[Block]
         while (index_col < nb_columns) and current_cell_is_valid:
             current_cell = optimized_table[index_row][index_col]
             current_cell_is_valid = _is_cell_valid(current_cell)
-            index_col += 1
+            if current_cell_is_valid:
+                index_col += 1
 
         index_invalid_cell = index_col
         index_row += 1
@@ -80,7 +73,8 @@ def get_least_blocks_from_blocks_table(blocks_table: BlocksTable) -> list[Block]
                 optimized_table,
                 index_row, top_left_cell_x, index_invalid_cell
             )
-            index_row += 1
+            if current_row_is_valid:
+                index_row += 1
 
         index_invalid_row = index_row
 
@@ -90,5 +84,8 @@ def get_least_blocks_from_blocks_table(blocks_table: BlocksTable) -> list[Block]
 
         blocks.append(block)
         _remove_block_from_table(block, optimized_table)
+
+        top_left_cell_x, top_left_cell_y = _find_coordinates_of_most_top_left_valid_cell(optimized_table)
+        index_row, index_col = top_left_cell_y, top_left_cell_x + 1
 
     return blocks
