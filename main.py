b5dca6e0ad26b1ef61741939f34f3792d156d5e2
Shaigro <robin.boucher@epita.fr>
2024-07-01 22:33:26 +0200

    qol: remove optimize button & display loader during optimization
    - optimization is automatically done when the file is loaded
    - display loader during optimization (it tries at the very least)

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ from browser import bind, window, document
 import misc
 
 load_btn = document["input_load_file"]
-optimize_btn = document["btn_optimize"]
 map_content = document["map_content"]
 span_before = document["before"]
 span_after = document["after"]
 span_ratio = document["ratio"]
-og_lines: list[str] = []
-optimized_lines: list[str] = []
 
-def clear_og_lines():
-    global og_lines
-    og_lines.clear()
 
 @bind(load_btn, "input")
 def file_read(ev):
@@ -21,17 +15,23 @@ def file_read(ev):
         event.target.
         The file content, as text, is the FileReader instance's "result"
         attribute."""
-        clear_og_lines()
+        og_lines = []
         lines = event.target.result.__str__().splitlines()
-        # optimized_lines = cli_main.optimize(lines)
-        # joined_string = "\n".join(optimized_lines)
-        # map_content.value = joined_string
         for line in lines:
             og_lines.append(line)
-        # enable the optimize button
-        del optimize_btn.attrs['disabled']
+        optimized_lines, stats = misc.optimize(og_lines)
 
-    optimize_btn.attrs["disabled"] = "true"
+        if stats['has_crashed']: # serializer manually using javascript
+            new_lines = []
+            for line in optimized_lines:
+                new_lines.append(window.JSONdumps(line))
+            optimized_lines = new_lines
+        joined_string = "\n".join(optimized_lines)
+        map_content.value = joined_string
+        span_before.textContent = stats["before"]
+        span_after.textContent = stats["after"]
+        span_ratio.textContent = stats["ratio"]
+
     # Get the selected file as a DOM File object
     file = load_btn.files[0]
     # Create a new DOM FileReader instance
@@ -39,21 +39,3 @@ def file_read(ev):
     # Read the file content as text
     reader.readAsText(file)
     reader.bind("load", onload)
-
-
-@bind(optimize_btn, "mousedown")
-def mousedown(evt):
-      """Create a "data URI" to set the downloaded file content
-      Cf. https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
-      """
-      optimized_lines, stats = misc.optimize(og_lines)
-      if stats['has_crashed']: # serializer manually using javascript
-        new_lines = []
-        for line in optimized_lines:
-            new_lines.append(window.JSONdumps(line))
-        optimized_lines = new_lines
-      joined_string = "\n".join(optimized_lines)
-      map_content.value = joined_string
-      span_before.textContent = stats["before"]
-      span_after.textContent = stats["after"]
-      span_ratio.textContent = stats["ratio"]
